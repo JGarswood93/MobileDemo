c1771aa2f5b6ba601197afb766cbd4c148a494bc
Jake <jacobbarton@rentalcars.com>
2019-07-18 10:30:45 +0100

    Initial commit

---
+++ src/main/java/com/rentalcars/mobiledemo/MobiledemoApplication.java
@@ -0,0 +1,13 @@
+package com.rentalcars.mobiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiledemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentalcars/mobiledemo/MobiledemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rentalcars.mobiledemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobiledemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
